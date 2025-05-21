58f414708452c99fd0eb781e56a54dab2c825f64
jinsh1210 <jinsh1210@naver.com>
2025-05-21 18:25:18 +0900

    update

--- src/utils/ClientSock.java
+++ src/utils/ClientSock.java
@@ -7,7 +7,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import models.FileInfo;
-import models.Repository;
 
 public class ClientSock {
    private static final String SERVERIP = "sjc07250.iptime.org";
